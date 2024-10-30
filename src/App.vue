4afe33bbbd21761069d74e1edee58abe9ee9d8ad
mengly74 <hourmengly74@gmail.com>
2024-10-30 07:37:18 +0700

    1. Create local variables to represent our components

--- src/App.vue
+++ src/App.vue
@@ -10,21 +10,84 @@ export default {
   data() {
     return {
       categories: [
-        { image: '/image/cakeandmilk.png', name: 'Cake & Milk', itemCount: 14 , backgroundColor: '#F2FCE4'},
-        { image: '/image/peach.png', name: 'Peach', itemCount: 17 , backgroundColor: '#FFFCEB'},
-        { image: '/image/kiwi.png', name: 'Oganic Kiwi', itemCount: 21 , backgroundColor: '#ECFFEC'},
-        { image: '/image/apple.png', name: 'Red Apple', itemCount: 68 , backgroundColor: '#FEEFEA'},
-        { image: '/image/snake.png', name: 'Snake', itemCount: 34 , backgroundColor: '#FFF3EB'},
-        { image: '/image/plum.png', name: 'Black Plum', itemCount: 25 , backgroundColor: '#FFF3FF'},
-        { image: '/image/cabbage.png', name: 'Vegetables', itemCount: 65 , backgroundColor: '#F2FCE4'},
-        { image: '/image/headphone.png', name: 'Headphone', itemCount: 34 , backgroundColor: '#FFFCEB'},
-        { image: '/image/milk&cake.png', name: 'Milk & Cake', itemCount: 53, backgroundColor: '#F2FCE4'},
-        { image: '/image/orange.png', name: 'Orange', itemCount: 63, backgroundColor: '#FFF3FF' },
+        {
+          image: '/image/cakeandmilk.png',
+          name: 'Cake & Milk',
+          itemCount: 14 ,
+          backgroundColor: '#F2FCE4'
+        },
+        { 
+          image: '/image/peach.png', 
+          name: 'Peach', 
+          itemCount: 17 , 
+          backgroundColor: '#FFFCEB'
+        },
+        { 
+          image: '/image/kiwi.png', 
+          name: 'Oganic Kiwi', 
+          itemCount: 21 , 
+          backgroundColor: '#ECFFEC'
+        },
+        { 
+          image: '/image/apple.png', 
+          name: 'Red Apple', 
+          itemCount: 68 , 
+          backgroundColor: '#FEEFEA'
+        },
+        { 
+          image: '/image/snake.png', 
+          name: 'Snake', 
+          itemCount: 34 , 
+          backgroundColor: '#FFF3EB'
+        },
+        { 
+          image: '/image/plum.png', 
+          name: 'Black Plum', 
+          itemCount: 25 , 
+          backgroundColor: '#FFF3FF'
+        },
+        { 
+          image: '/image/cabbage.png', 
+          name: 'Vegetables', 
+          itemCount: 65 , 
+          backgroundColor: '#F2FCE4'},
+        { 
+          image: '/image/headphone.png', 
+          name: 'Headphone', 
+          itemCount: 34 , 
+          backgroundColor: '#FFFCEB'
+        },
+        { 
+          image: '/image/milk&cake.png', 
+          name: 'Milk & Cake', 
+          itemCount: 53, 
+          backgroundColor: '#F2FCE4'
+        },
+        { 
+          image: '/image/orange.png', 
+          name: 'Orange', 
+          itemCount: 63, 
+          backgroundColor: '#FFF3FF' 
+        },
       ],
       promotions: [
-        { title: 'Everyday Fresh & Clean with Our Products', image: '/image/onion.png', width: 200,backgroundColor: '#F0E8D5'},
-        { title: 'Make your Breakfast Healthy and Easy', image: '/image/milk.png', width: 150, backgroundColor: '#F3E8E8'},
-        { title: 'The best Organic Products Online', image: '/image/vegetables.png', width: 240, backgroundColor: '#E7EAF3'}
+        { 
+          title: 'Everyday Fresh & Clean with Our Products', 
+          image: '/image/onion.png', 
+          width: 200,
+          backgroundColor: '#F0E8D5'
+        },
+        { 
+          title: 'Make your Breakfast Healthy and Easy', 
+          image: '/image/milk.png', 
+          width: 150, 
+          backgroundColor: '#F3E8E8'},
+        { 
+          title: 'The best Organic Products Online', 
+          image: '/image/vegetables.png', 
+          width: 240, 
+          backgroundColor: '#E7EAF3'
+        }
       ]
     }
   }
