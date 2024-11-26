54d3bf7e818e76caa449261196f24c7e732be97a
mengly74 <hourmengly74@gmail.com>
2024-11-26 21:12:24 +0700

    exercise 3

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,54 @@
 <script setup>
-import { onMounted, computed } from "vue";
+import { onMounted, computed, ref } from "vue";
 import { useProductStore } from "./stores/productStore";
 import Category from "./components/Category.vue";
 import Promotion from "./components/Promotion.vue";
-import FeatureCategory from "./components/FeatureCategory.vue";
+import MenuComponent from "./components/MenuComponent.vue";
+import Product from "./components/Product.vue";
 
 const productStore = useProductStore();
 const filteredCategories = computed(() => productStore.filteredCategories);
-const selectGroup = (group) => {
-  productStore.setSelectedGroup(group);
+const categoryByGroup = (group) => {
+  productStore.setSelectedGroup(group); 
+  console.log("Selected group:", group);
 };
+const productByGroup = (group) => {
+  selectedGroup.value = group;
+  console.log("Selected group for products:", group);
+};
+const selectedGroup = ref("");
 
 onMounted(async () => {
   await productStore.fetchCategories();
   await productStore.fetchPromotions();
   await productStore.fetchGroups();
+  await productStore.fetchProducts();
   console.log("Categories loaded:", productStore.categories);
   console.log("Promotions loaded:", productStore.promotions);
+  console.log("Products loaded:", productStore.products);
   console.log("Groups loaded:", productStore.groups);
+});
+const categoryTitle = "Featured Categories";
+const productTitle = "Popular Products";
+const products = computed(() => {
+  if (selectedGroup.value === "") {
+    return productStore.products; // Show all products if no group is selected
+  }
+  return productStore.getProductsByGroup(selectedGroup.value); // Filtered products
 });
 </script>
 
 <template>
   <div id="app">
-    <div class="container">
-      <h1 class="name"> Featured Categories</h1>
-      <div  class="list-cate">
-        <FeatureCategory v-for="(group, index) in productStore.groups" 
-        :key="index" 
-        :name="group"
-        @click="() => selectGroup(group)" />
-      </div>
+    <!-- === Feature Category === -->
+    <div>
+      <MenuComponent
+      :name = "categoryTitle"
+      :group = "productStore.groups"
+      @group-select = "categoryByGroup"
+      />
     </div>
-
+    <!-- === Catefory === -->
     <div class="category-card">
       <Category v-for="(filter, index) in filteredCategories" 
       :key="index" 
@@ -42,7 +58,7 @@ onMounted(async () => {
       :color="filter.color"
       />
     </div>
-
+    <!-- === Promotion === -->
     <div class="promotion-section">
       <Promotion v-for="(promotion, index) in productStore.promotions" 
         :key="index" 
@@ -53,13 +69,46 @@ onMounted(async () => {
         :width="promotion.width"
         />
     </div>
+    <!-- === Popular Product === -->
+    <div>
+      <MenuComponent
+      :name = "productTitle"
+      :group = "productStore.groups"
+      @group-select = "productByGroup"
+      />
+    </div>
+    <div>
+      <Product
+      :products="products"
+      />
+    </div>
   </div>
 </template>
 
 <style scoped>
 #app {
+  width: 2850px;
+  height: auto;
+  padding-bottom: 1000px;
   display: flex;
   flex-direction: column;
+}
+
+.container{
+  /* background-color: antiquewhite; */
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 3%;
+  margin-bottom: 3%;
+}
+
+.name{
+  /* justify-content: start;
+  align-items: center ; */
+  /* padding-right: 24%; */
+  color: #253D4E;
+  font-weight: bold;
 }
 
 .category-card {
---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="list-cate">
+        <h3>{{ name }}</h3>
+        <div class="group">
+            <p v-for="(item, index) in group" :key="index" @click="$emit('group-select', item)">{{ item }}</p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        group: {
+            type: String,
+            required: true,
+        }
+    },
+};
+</script>
+
+<style scoped>
+.list-cate {
+    padding: 10px;
+    width: 100px;
+    cursor: pointer;
+    display: flex;
+    justify-content: space-between;
+    /* color: #253D4E; */
+    /* background-color: #f0f0f0; */
+    /* border-radius: 8px; */
+}
+
+.list-cate h3 {
+    color: #253D4E;
+    font-weight: bold;
+}
+
+.group{
+    display: flex;
+    gap: 25px;
+    color: #253D4E;
+}
+/* .name {
+    color: #253D4E;
+    font-weight: bold;
+} */
+</style>
